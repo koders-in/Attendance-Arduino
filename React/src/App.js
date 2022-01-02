9d398150023f1128bffbdf3579072f9400643d08
Saksham <sakshamchauhan79@gmail.com>
2022-01-02 13:33:08 +0530

    removed the dummy data from app.js file

--- React/src/App.js
+++ React/src/App.js
@@ -9,29 +9,7 @@ export const UseContext = createContext("");
 
 function App() {
   const [mode, setmode] = useState("light");
-  const [user, setUser] = useState({
-    user: {
-      name: "Saksham Chauhan",
-      user_id: 72,
-    },
-    project: {
-      opened: 2,
-      total: 2,
-    },
-    issue: {
-      opened: 3,
-      total: 18,
-    },
-    spent_time: {
-      "31/12/2021": 0,
-      "30/12/2021": 8.0,
-      "29/12/2021": 0,
-      "28/12/2021": 8.0,
-      "27/12/2021": 8.0,
-      "26/12/2021": 0,
-      "25/12/2021": 0,
-    },
-  });
+  const [user, setUser] = useState({});
   const [data, setData] = useState({
     username: "",
     password: "",
@@ -48,10 +26,10 @@ function App() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // const params = data;
-    // const result = await getRequest({ params });
-    // setUser(result.data);
-    // console.log(user);
+    const params = data;
+    const result = await getRequest({ params });
+    setUser(result.data);
+    console.log(user);
   };
   const handleMode = () => {
     setmode((pre) => (pre === "light" ? "dark" : "light"));
@@ -62,11 +40,9 @@ function App() {
       return { ...pre, [name]: value };
     });
   };
-  {
-    /* <Login {...{ handleChange, handleSubmit }} /> */
-  }
+
   if (Object.keys(user).length === 0) {
-    return <Dashboard {...{ user }} />;
+    return <Login {...{ handleChange, handleSubmit }} />;
   }
   return (
     <UseContext.Provider value={{ handleMode }}>
