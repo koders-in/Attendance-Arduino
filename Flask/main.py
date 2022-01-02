09b4673411b56a446f4dc82a8e8e671d0d22a831
xhunter <kartikey@koders.in>
2022-01-02 16:09:35 +0530

    Fixed getting attendance data from gql

--- Flask/main.py
+++ Flask/main.py
@@ -3,16 +3,14 @@ from datetime import datetime, time
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import asyncio
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), 'env')
+print(dotenv_path)
 load_dotenv(dotenv_path)
 
-URL = os.environ.get("HASURA_URL")
-PASSWORD = os.environ.get("SECRET_KEY")
-
-HASURA_URL = URL
-HASURA_HEADERS = {
-    "X-Hasura-Admin-Secret": PASSWORD}
+HASURA_URL = os.environ.get("HASURA_URL")
+HASURA_HEADERS = {"X-Hasura-Admin-Secret": os.environ.get("SECRET_KEY")}
 
 MORN_SHIFT_START = 7260
 MORN_SHIFT_END = 48600
@@ -39,7 +37,7 @@ def insert_attendance(_id: int, time: str):
             # Post clock_in in the dawn table
             client.post_time(_id, time, current_date, status, 'clock_in')
     else:
-        status = 'dusk'
+        status = 'dusk'  # Unused variable
         # Clock_in in dawn == YES
         # Clock_out in dawn == NO
         if has_duplicates(_id, 'dawn', 'clock_in') and has_duplicates(_id, 'dawn', 'clock_out') == False:
@@ -52,34 +50,35 @@ def insert_attendance(_id: int, time: str):
 
         # Clock_in in dawn == NO
         # Clock_in in dusk == NO
-        if has_duplicates(_id, 'dusk', 'clock_in') == False:
+        if not has_duplicates(_id, 'dusk', 'clock_in'):
             client.post_time(_id, time, current_date, 'dusk', 'clock_in')
 
 
-def time_to_num(time_str: str) -> int:          # Convert time to seconds
+def time_to_num(time_str: str) -> int:  # Convert time to seconds
     hh, mm, ss = map(int, time_str.split(':'))
-    return ss + 60*(mm + 60*hh)
+    return ss + 60 * (mm + 60 * hh)
 
 
 def has_duplicates(_id: int, table_name: str, status: str) -> bool:
     data = client.fetch_by_id(_id, table_name, status)
-    extracted_time = data['data'][table_name+'_by_pk']
+    extracted_time = data['data'][table_name + '_by_pk']
     if extracted_time is None:
         return False
     else:
         return True
 
 
-def cooldown(_id: int, time: str,  table_name: str) -> bool:
+def cooldown(_id: int, cooldown_timer: str, table_name: str) -> bool:
     fetched_time = client.fetch_by_id(id, table_name)
-    formatted_time = fetched_time['data'][table_name+'_by_pk']['clock_in']
+    formatted_time = fetched_time['data'][table_name + '_by_pk']['clock_in']
     previous_time = time_to_num(formatted_time)
-    current_time = time_to_num(time)
+    current_time = time_to_num(cooldown_timer)
     if current_time - previous_time < COOLDOWN_INTERVAL:
         return True
     else:
         return False
 
 
-def get_attendance(_id: int):
-    print(client.query_for_all(_id, 'dawn'))
+def get_attendance(_id: str):
+    print(client.query_for_all(_id))
+
--- Flask/util.py
+++ Flask/util.py
@@ -7,6 +7,10 @@ class Client:
     url: str
     headers: dict
 
+    def __init__(self, url, headers):
+        self.url = url
+        self.headers = headers
+
     # def run_query(self, query: str, variables: dict, extract=False):
     def run_query(self, query: str, variables: dict = None):
         request = requests.post(
@@ -22,7 +26,7 @@ class Client:
         self.run_query(
             """
         mutation ($_id: Int!, $clock: time!, $day: time!) {
-            insert_"""+table_name+"""_one(objects: {_id: $_id, """ + status + """: $clock, day: $day}) {
+            insert_""" + table_name + """_one(objects: {_id: $_id, """ + status + """: $clock, day: $day}) {
                 affected_rows
                 returning {
                     _id
@@ -37,28 +41,49 @@ class Client:
 
     # Fetch clock_in or clock_out time for a given id
     def fetch_by_id(self, _id: int, table_name: str, status: str):
-
         return self.run_query(
             """
                 query fetch_by_id($_id: Int!) {
-                    """+table_name+"""_by_pk(_id: $_id) {
-                        """+status+"""
+                    """ + table_name + """_by_pk(_id: $_id) {
+                        """ + status + """
                     }
                 }
             """,
             {'_id': _id}
         )
 
-    def query_for_all(self, _id: int, table_name: str):
+    def query_for_all(self, _id: str):
         return self.run_query(
             """
-                query fetch_by_id($_id: Int!) {
-                    """+table_name+"""_by_pk(_id: $_id) {
+            query fetch_by_id {
+                    dawn(where: {user_id: {_eq: """ + _id + """ }}, order_by: {date: desc}) {
+                        id
+                        user_id
+                        date
                         clock_in
                         clock_out
-                        date
+                    }
+                    dusk(where: {user_id: {_eq: """ + _id + """ }}, order_by: {date: desc}) {
+                    id
+                    user_id
+                    date
+                    clock_in
+                    clock_out
                     }
                 }
-            """,
-            {'_id': _id}
+            """
         )
+
+
+        # return self.run_query(
+        #     """
+        #         query fetch_by_id($_id: Int!) {
+        #             """+table_name+"""_by_pk(_id: $_id) {
+        #                 clock_in
+        #                 clock_out
+        #                 date
+        #             }
+        #         }
+        #     """,
+        #     {'_id': _id}
+        # )
