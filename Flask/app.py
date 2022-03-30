2e30d13806ebb14a9a6b43447c93a2ed251ba3a1
Basudev Tyagi <tyagi101basudev@gmail.com>
2022-03-30 20:25:57 +0530

    commented code for base endpoint

--- Flask/app.py
+++ Flask/app.py
@@ -26,71 +26,71 @@ def process_attendance(_id):
         return insert_attendance(_id, _time)
 
 
-@app.route('/', methods=['POST', 'GET'])
-@cross_origin()
-def send_user_data():
-    if request.method == 'GET':
-        return "Server is running... Hello world!"
-    if request.method == 'POST':
-        if not request.get_json():
-            return "Something went wrong."
-        username = request.get_json()["username"]
-        password = request.get_json()["password"]
-
-        redmine = Redmine('https://kore.koders.in', username=username, password=password)
-
-        user = {
-            "name": str(redmine.user.get('me')),
-            "user_id": redmine.user.get('me').id
-        }
-
-        # PROJECTS
-        projects = redmine.project.all(limit=100)
-        projects_opened = 0
-        for project in projects:
-            if project.status:
-                projects_opened += 1
-        project = {
-            "opened": projects_opened,
-            "total": projects.total_count
-        }
-
-        # ISSUES
-        user_id = redmine.user.get('me').id
-        issues = redmine.issue.all(limit=100, assigned_to_id=user_id)
-        opened_issues, total_issues = 0, 0
-        for issue in issues:
-            if str(issue.status) != 'Closed':
-                opened_issues += 1
-            total_issues += 1
-
-        issues_dict = {
-            "opened": opened_issues,
-            "total": total_issues
-        }
-
-        # SPENT TIME LAST 7 DAYS
-        spent_time = {}
-        week = []
-        for i in range(0, 7):
-            week.append(((datetime.datetime.now() - datetime.timedelta(days=i)).date()))
-
-        for day in week:
-            time_entries = redmine.time_entry.filter(user_id=user_id, spent_on=day)
-            hours = 0
-            for x in time_entries:
-                hours += x.hours
-            spent_time[day.strftime("%d/%m/%Y")] = hours
-
-        data = {
-            "user": user,
-            "project": project,
-            "issue": issues_dict,
-            "spent_time": spent_time
-        }
-
-        json_data = json.dumps(data, indent=4)
-        return json_data
+# @app.route('/', methods=['POST', 'GET'])
+# @cross_origin()
+# def send_user_data():
+#     if request.method == 'GET':
+#         return "Server is running... Hello world!"
+#     if request.method == 'POST':
+#         if not request.get_json():
+#             return "Something went wrong."
+#         username = request.get_json()["username"]
+#         password = request.get_json()["password"]
+#
+#         redmine = Redmine('https://kore.koders.in', username=username, password=password)
+#
+#         user = {
+#             "name": str(redmine.user.get('me')),
+#             "user_id": redmine.user.get('me').id
+#         }
+#
+#         # PROJECTS
+#         projects = redmine.project.all(limit=100)
+#         projects_opened = 0
+#         for project in projects:
+#             if project.status:
+#                 projects_opened += 1
+#         project = {
+#             "opened": projects_opened,
+#             "total": projects.total_count
+#         }
+#
+#         # ISSUES
+#         user_id = redmine.user.get('me').id
+#         issues = redmine.issue.all(limit=100, assigned_to_id=user_id)
+#         opened_issues, total_issues = 0, 0
+#         for issue in issues:
+#             if str(issue.status) != 'Closed':
+#                 opened_issues += 1
+#             total_issues += 1
+#
+#         issues_dict = {
+#             "opened": opened_issues,
+#             "total": total_issues
+#         }
+#
+#         # SPENT TIME LAST 7 DAYS
+#         spent_time = {}
+#         week = []
+#         for i in range(0, 7):
+#             week.append(((datetime.datetime.now() - datetime.timedelta(days=i)).date()))
+#
+#         for day in week:
+#             time_entries = redmine.time_entry.filter(user_id=user_id, spent_on=day)
+#             hours = 0
+#             for x in time_entries:
+#                 hours += x.hours
+#             spent_time[day.strftime("%d/%m/%Y")] = hours
+#
+#         data = {
+#             "user": user,
+#             "project": project,
+#             "issue": issues_dict,
+#             "spent_time": spent_time
+#         }
+#
+#         json_data = json.dumps(data, indent=4)
+#         return json_data
 
 
 if __name__ == '__main__':
