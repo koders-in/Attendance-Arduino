55d1c351df5c035408b9a265bbe9e6f5b8763e92
XHunter <kushwah.kartikey14@upesalumni.upes.ac.in>
2023-03-28 10:55:50 +0530

    move(card-reader): move card reader in parent
    Signed-off-by: XHunter <kushwah.kartikey14@upesalumni.upes.ac.in>

--- card_reader.ino
+++ card_reader.ino
@@ -5,24 +5,24 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-#define LED D1 // Led in NodeMCU at pin GPIO16 (D0).
-#define RST_PIN D0 // Configurable, see typical pin layout above
-#define SS_PIN D8 // Configurable, see typical pin layout above
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+#define LED D1
+#define RST_PIN D0 
+#define SS_PIN D8
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
 MFRC522::MIFARE_Key key;
 MFRC522::StatusCode status;
 
  
-const char *WIFI_SSID = "Koders"; // Your SSID goes here
-const char *WIFI_PASSWORD = "KodersKorp@12344321"; // Your wifi password goes here
-const char *URL = "http://1:9020"; // Your backend link goes here
+const char *WIFI_SSID = "SSID"; 
+const char *WIFI_PASSWORD = "PASSWORD"; 
+const char *URL = "http://HOST:PORT"; 
 
 WiFiClient client;
 HTTPClient httpClient;
 //*****************************************************************************************//
 void setup() {
-    pinMode(LED, OUTPUT); // set the digital pin as output.
-    Serial.begin(9600); // Initialize serial communications with the PC
+    pinMode(LED, OUTPUT);
+    Serial.begin(9600);
     
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
@@ -31,26 +31,22 @@ void setup() {
     }
     Serial.println("Connected to the Wifi");
     
-    SPI.begin(); // Init SPI bus
-    mfrc522.PCD_Init(); // Init MFRC522 card
-    Serial.println(F("Waiting to read personal data on a MIFARE PICC:")); //shows in serial that it is ready to read
+    SPI.begin(); 
+    mfrc522.PCD_Init(); 
+    Serial.println(F("Waiting to read Employee Card:")); 
 }
 //*****************************************************************************************//
 void loop() {
-    // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
     for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
     byte block;
     byte len;
-    //-------------------------------------------
-    // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
     if (!mfrc522.PICC_IsNewCardPresent()) {
         return;
     }
-    // Select one of the cards
     if (!mfrc522.PICC_ReadCardSerial()) {
         return;
     }
-    Serial.println(F("**Card Detected:**"));
+    Serial.println(F("**Employee Card Detected:**"));
     byte buffer1[18];
     block = 4;
     len = 18;
@@ -73,61 +69,53 @@ void loop() {
     value.trim();
     Serial.print(value);
     Serial.println(F("\n**End Reading**\n"));
-    delay(1000);
     mfrc522.PICC_HaltA();
     mfrc522.PCD_StopCrypto1();
 
     String payload = "{\"user_id\": \"" + value + "\"}";
-    
     httpClient.begin(client, URL);
     httpClient.addHeader("Content-Type", "application/json");
     int statusCode = httpClient.POST(payload);
-    Serial.print(statusCode);
-    Serial.print(payload);
     String content = httpClient.getString();
     httpClient.end();
-
-    Serial.print(content);
+    
     if (statusCode == 200){
       if (content == "cooldown initiated. try again later."){
-      digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually
-      delay(100);          // wait for 1 second.
+      digitalWrite(LED, HIGH);
+      delay(100);          
       digitalWrite(LED, LOW);
       delay(100);
 
-      digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually
-      delay(100);          // wait for 1 second.
+      digitalWrite(LED, HIGH);
+      delay(100);          
       digitalWrite(LED, LOW);
       delay(100);
 
-      digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually
-      delay(100);          // wait for 1 second.
+      digitalWrite(LED, HIGH);
+      delay(100);          
       digitalWrite(LED, LOW);
       delay(100);
 
-      digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually
-      delay(100);          // wait for 1 second.
+      digitalWrite(LED, HIGH);
+      delay(100);
       digitalWrite(LED, LOW);
       delay(100);
 
-      digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually
-      delay(100);          // wait for 1 second.
+      digitalWrite(LED, HIGH);
+      delay(100);          
       digitalWrite(LED, LOW);
       delay(100);
 
-      digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually
-      delay(100);          // wait for 1 second.
+      digitalWrite(LED, HIGH);
+      delay(100);          
       digitalWrite(LED, LOW);
       delay(100);
-
-      
-      }
-      else{
-      digitalWrite(LED, HIGH);// turn the LED off.(Note that LOW is the voltage level but actually
-      delay(2000);          // wait for 1 second.
-      digitalWrite(LED, LOW);
       }
     }
-
+    else {
+    digitalWrite(LED, HIGH);
+    delay(2000);
+    digitalWrite(LED, LOW);
     delay(1000);
+    }
 }
